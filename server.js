0539d2516bc5662e2b2feec186fb15971f77cfb6
Lazy-Code-X01 <lazycode668@gmail.com>
2024-03-08 23:33:33 +0100

    cors update

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ import path from "path";
 import express, { json } from "express";
 import dotenv from "dotenv";
 dotenv.config();
+import cors from "cors";
 import userRoutes from "./routes/userRoutes.js";
 import { notFound, errorHandler } from "./middleware/userMiddleware.js";
 import connectDB from "./config/db.js";
@@ -11,7 +12,7 @@ const port = process.env.PORT || 5000;
 connectDB();
 
 const app = express();
-
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
@@ -20,13 +21,13 @@ app.use(cookieParser());
 app.use("/api/users", userRoutes);
 
 if (process.env.NODE_ENV === "production") {
-	const _dirname = path.resolve();
-	app.use(express.static(path.join(__dirname, "frontend/dist")));
-	app.get("*", (req, res) =>
-		res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html")),
-	);
+  const _dirname = path.resolve();
+  app.use(express.static(path.join(__dirname, "frontend/dist")));
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"))
+  );
 } else {
-	app.get("/", (req, res) => res.send("Server is ready"));
+  app.get("/", (req, res) => res.send("Server is ready"));
 }
 
 app.use(notFound);
